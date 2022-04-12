66f60fe4d1b0c3072c8b04ec17a40ce0825cc97c
Felipe Lazzarini <felipe.mumei@gmail.com>
2022-04-11 23:22:11 -0300

    Tratando erros com excecao personalizada

--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -13,14 +13,43 @@
             this.colunas = colunas;
             pecas = new Peca[linhas,colunas];
         }
-        public Peca peca(int linha, int coluna) //Método para retornar uma peça em dada linha e coluna para usar na tela
+        public Peca peca(int linha, int coluna) // Método para retornar uma peça em dada linha e coluna para usar na tela
         {
             return pecas[linha,coluna];
         }
+        public Peca peca(Posicao pos) // Método para retornar uma peça dada sua posição (sobrecarga do método anterior)
+        {
+            return pecas[pos.linha,pos.coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos)!= null;
+        }
         public void colocarPeca(Peca p, Posicao pos)
         {
-            pecas[pos.linha,pos.coluna] = p;
-            p.posicao = pos;
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!"); // Dessa forma testamos se a posição é válida e se já existe uma peça nessa posição
+            }
+            pecas[pos.linha,pos.coluna] = p; //Controle do objeto (no programa principal nos testes chamado de 'tab') para acossiar uma peça a uma posição
+            p.posicao = pos; //Atualiza a posição que a peça p está (começa com o valor null) para a própria peça
+        }
+        public bool posicaoValida(Posicao pos) // Verifica se dada posição é válida
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
