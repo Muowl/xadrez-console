8d9d5d6896f53b9e163eae1d78099aba6d783122
Felipe Lazzarini <felipe.mumei@gmail.com>
2022-04-13 02:47:40 -0300

    Testes iniciais com movimentos possiveis das pecas

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -13,5 +13,68 @@ namespace xadrez
         {
             return "R";
         }
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;// Cor do rei this.cor
+
+        }
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0,0);
+
+            // Acima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            if (tab.posicaoValida(pos) && podeMover(pos)) // Verificando se posição é válida e se o espaço esta ocupado por alguma peça ou peça inimiga (cor diferente do rei)
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            // Nordeste
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) // Verificando se posição é válida e se o espaço esta ocupado por alguma peça ou peça inimiga (cor diferente do rei)
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            // Direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) // Verificando se posição é válida e se o espaço esta ocupado por alguma peça ou peça inimiga (cor diferente do rei)
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            // Suldeste
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) // Verificando se posição é válida e se o espaço esta ocupado por alguma peça ou peça inimiga (cor diferente do rei)
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            // Abaixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            if (tab.posicaoValida(pos) && podeMover(pos)) // Verificando se posição é válida e se o espaço esta ocupado por alguma peça ou peça inimiga (cor diferente do rei)
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            // Sudoeste
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) // Verificando se posição é válida e se o espaço esta ocupado por alguma peça ou peça inimiga (cor diferente do rei)
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            // Esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) // Verificando se posição é válida e se o espaço esta ocupado por alguma peça ou peça inimiga (cor diferente do rei)
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            // Noroeste
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos)) // Verificando se posição é válida e se o espaço esta ocupado por alguma peça ou peça inimiga (cor diferente do rei)
+            {
+                mat[pos.linha, pos.coluna] = true;
+            }
+            return mat;
+        }
+
     }
 }
--- xadrez-console/xadrez/Torre.cs
+++ xadrez-console/xadrez/Torre.cs
@@ -13,5 +13,65 @@ namespace xadrez
         {
             return "T";
         }
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;// Cor do rei this.cor
+
+        }
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao (0,0);
+
+            // Acima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true; // Enquanto a posição for válida e podermos mover a peça, adicionamos o valor true aquela matriz de bool
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break; // Forca a parada do while se encontra uma peça inimiga, para que uma peça não consiga pular uma peça e capturar a anterior
+                }
+                pos.linha = pos.linha - 1;
+            }
+            // Abaixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break; // Forca a parada do while se encontra uma peça inimiga
+                }
+                pos.linha = pos.linha + 1;
+            }
+            // Direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break; // Forca a parada do while se encontra uma peça inimiga
+                }
+                pos.coluna = pos.coluna + 1;
+            }
+            // Esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break; // Forca a parada do while se encontra uma peça inimiga
+                }
+                pos.coluna = pos.coluna - 1;
+            }
+
+            return mat;
+        }
+
     }
 }
